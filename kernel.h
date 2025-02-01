5936f81495ab8b531f0062ae702a07e2d2e779c6
Seth Wood <sethlwood@me.com>
2025-02-01 15:17:09 -0800

    creating process functionality

--- kernel.h
+++ kernel.h
@@ -2,6 +2,11 @@
 
 #include "common.h"
 
+#define PROCS_MAX 8       // Maximum number of processes
+
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
 #define PANIC(fmt, ...)                                                        \
 do {                                                                       \
 printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
@@ -59,3 +64,10 @@ struct trap_frame {
     uint32_t s11;
     uint32_t sp;
 } __attribute__((packed));
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
